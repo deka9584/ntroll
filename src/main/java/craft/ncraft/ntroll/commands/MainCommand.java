7e25c86472ceaf3602c437f116e2385cc015622c
open-antux <antonio.giustino@proton.me>
2026-03-10 19:28:44 +0100

    Fixed function signatures

--- src/main/java/craft/ncraft/ntroll/commands/MainCommand.java
+++ src/main/java/craft/ncraft/ntroll/commands/MainCommand.java
@@ -13,46 +13,42 @@ import craft.ncraft.ntroll.utils.Utils;
 public class MainCommand implements CommandExecutor {
     private final NTroll plugin;
     private final Utils utils;
-    private String noperm;
 
     public MainCommand(NTroll plugin) {
         this.plugin = plugin;
         this.utils = plugin.getUtils();
-        this.noperm = utils.getMsgFromCfg("no-permission-msg");
     }
 
     @Override
     public boolean onCommand(CommandSender cs, Command command, String label, String[] args) {
         String permission = command.getPermission();
+        String noperm = utils.getMsgFromCfg("no-permission-msg");
 
         if (cs instanceof Player || cs instanceof ConsoleCommandSender) {
             if (args.length == 0) {
                 cs.sendMessage(plugin.getName() + " v." +
-
                         plugin.getDescription().getVersion());
                 return true;
             }
 
             switch (args[0].toLowerCase()) {
                 case "reload":
-                    if (!hasPermission(cs, permission + ".admin"))
+                    if (!hasPermission(cs, permission + ".admin", noperm))
                         return false;
                     plugin.reload();
                     cs.sendMessage(utils.getMsgFromCfg("config-reloaded-msg"));
                     return true;
 
-                case "enable":
                 case "on":
-                    if (!hasPermission(cs, permission + ".admin") || hasTrollEnable(cs, false))
+                    if (!hasPermission(cs, permission + ".admin", noperm) || checkTrollBeEnable(cs, false))
                         return false;
                     plugin.getConfig().set("enable-troll", true);
                     plugin.saveConfig();
                     cs.sendMessage(utils.getMsgFromCfg("troll-enabled-msg"));
                     return true;
 
-                case "disable":
                 case "off":
-                    if (!hasPermission(cs, permission + ".admin") || hasTrollEnable(cs, true))
+                    if (!hasPermission(cs, permission + ".admin", noperm) || checkTrollBeEnable(cs, true))
                         return false;
                     plugin.getConfig().set("enable-troll", false);
                     plugin.saveConfig();
@@ -60,13 +56,14 @@ public class MainCommand implements CommandExecutor {
                     return true;
 
                 case "status":
-                    if (!hasPermission(cs, permission + ".admin"))
+                    if (!hasPermission(cs, permission + ".admin", noperm))
                         return false;
                     cs.sendMessage(utils.getMsgFromCfg("troll-status-msg")
                             .replaceAll("%status%", plugin.isTrollEnabled() ? "ON" : "OFF"));
+                    return true;
 
                 case "add":
-                    if (!hasPermission(cs, permission + ".admin"))
+                    if (!hasPermission(cs, permission + ".admin", noperm))
                         return false;
                     if (args.length == 1) {
                         cs.sendMessage(ChatColor.RED + "Usage: add [player]");
@@ -77,9 +74,10 @@ public class MainCommand implements CommandExecutor {
                                 .replaceAll("%player%", args[1]));
                         return true;
                     }
+                    return false;
 
                 case "remove":
-                    if (!hasPermission(cs, permission + ".admin"))
+                    if (!hasPermission(cs, permission + ".admin", noperm))
                         return false;
                     if (args.length == 1) {
                         cs.sendMessage(ChatColor.RED + "Usage: remove [player]");
@@ -90,9 +88,10 @@ public class MainCommand implements CommandExecutor {
                                 .replaceAll("%player%", args[1]));
                         return true;
                     }
+                    return false;
 
                 case "list":
-                    if (!hasPermission(cs, permission + ".admin"))
+                    if (!hasPermission(cs, permission + ".admin", noperm))
                         return false;
                     String targetPlayers = String.join(" ,", plugin.getTargetPlayerManager()
                             .getPlayers());
@@ -101,7 +100,7 @@ public class MainCommand implements CommandExecutor {
                     return true;
 
                 case "break-actions":
-                    if (!hasPermission(cs, permission + ".admin"))
+                    if (!hasPermission(cs, permission + ".admin", noperm))
                         return false;
                     String breakActions = String.join("\n", plugin.getUnluckyBlocksManager()
                             .getActionsList());
@@ -115,6 +114,7 @@ public class MainCommand implements CommandExecutor {
                             "Subcommands: on, off, status, add [player], remove [player], " +
                             "list, break-actions");
                     return true;
+
                 default:
                     cs.sendMessage(ChatColor.RED + "Invalid subcommand");
                     return false;
@@ -124,7 +124,7 @@ public class MainCommand implements CommandExecutor {
         return false;
     }
 
-    private boolean hasPermission(CommandSender cs, String permission) {
+    private boolean hasPermission(CommandSender cs, String permission, String noperm) {
         if (!cs.hasPermission(permission + ".admin")) {
             cs.sendMessage(noperm);
             return false;
@@ -133,15 +133,15 @@ public class MainCommand implements CommandExecutor {
         return true;
     }
 
-    private boolean hasTrollEnable(CommandSender cs, boolean mustBeEnable) {
-        if (plugin.isTrollEnabled() ^ mustBeEnable) {
+    private boolean checkTrollBeEnable(CommandSender cs, boolean shouldBeEnable) {
+        if (plugin.isTrollEnabled() ^ shouldBeEnable) {
             plugin.debugLog("Troll plugin already " +
                     (plugin.isTrollEnabled() ? "enabled" : "disabled"));
             cs.sendMessage(utils.getMsgFromCfg("troll-status-msg")
-                    .replaceAll("%status%", "ON"));
-            return false;
+                           .replaceAll("%status%", plugin.isTrollEnabled() ? "ON" : "OFF"));
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
